42649a59cd62758b61181869f7525d9cb0fc7e2d
Rahulpuram <77501084+Rahulpuram@users.noreply.github.com>
2022-11-25 10:55:19 +0530

    Updated Test.py

--- Test.py
+++ Test.py
@@ -1,19 +1,19 @@
 import unittest
 from Factorial import factorial
 class Testfactorial(unittest.TestCase):
-    def test_fact1(self):
+    def test_case_1(self):
         result = factorial(5)
         self.assertEqual(result,120)
-    def test_fact2(self):
+    def test_case_2(self):
         result = factorial(6)
         self.assertEqual(result,720)
-    def test_fact3(self):
+    def test_case_3(self):
         result = factorial(7)
         self.assertEqual(result,5040)
-    def test_fact4(self):
+    def test_case_4(self):
         result = factorial(4)
         self.assertEqual(result,23)
-    def test_fact5(self):
+    def test_case_5(self):
         result = factorial(3)
         self.assertEqual(result,5)
 
